baa80ad1d74f5c288443bbb06b9d4180d0ee08f9
Valentinos <poupouxios@gmail.com>
2015-12-02 19:42:31 +0000

    first commit to initialize the files to install the module

---
+++ include/mapper/InstragramBookMapper.php
@@ -0,0 +1,10 @@
+<?php
+
+	class InstagramBookMapper extends InstagramBookBaseMapper{
+
+		public function __construct(){
+			$this->tableName = "instagram_book";
+		}
+		
+	}
+?>
---
+++ include/model/InstragramBookModel.php
@@ -0,0 +1,13 @@
+<?php
+
+	class InstagramBookModel extends InstagramBookBaseModel{
+		public static $attributes = array(
+			"id" => 0,
+			"instagram_api_key" => "",
+			"created_at" => "NOW()",
+			"updated_at" => "NOW()"
+		);
+
+	}
+
+?>
